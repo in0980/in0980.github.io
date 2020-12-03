ef8ff5ec4b33319101336bcdc519ea23a27fb6f0
Leopoldo Teixeira <lmt@cin.ufpe.br>
2020-12-03 10:23:53 -0300

    updating python example

--- 2020-12-03/py-coverage/area.py
+++ 2020-12-03/py-coverage/area.py
@@ -1,8 +1,21 @@
 from math import pi
-
 def circle_area(r):
     if type(r) not in [int, float]:
         raise TypeError("The radius must be a non-negative real number.")
     if r < 0:
         raise ValueError("The radius must not be negative.")
     return pi*(r**2)
+
+def rectangle_area(w,l):
+    if type(w) not in [int, float]:
+        raise TypeError("The width must be a non-negative real number.")
+    if w < 0:
+        raise ValueError("The width must not be negative.")
+    if type(l) not in [int, float]:
+        raise TypeError("The length must be a non-negative real number.")
+    if l < 0:
+        raise ValueError("The length must not be negative.")    
+    return w*l
+
+def square_area(a):
+    return rectangle_area(a,a)
--- 2020-12-03/py-coverage/tests.py
+++ 2020-12-03/py-coverage/tests.py
@@ -7,17 +7,10 @@ class TestCircleArea(unittest.TestCase):
         #Test area when radius >= 0
         self.assertAlmostEqual(circle_area(1), pi)
 
-'''
     def test_values(self):
         #Make sure value errors are raised when necessary
         self.assertRaises(ValueError, circle_area, -2)
-'''
 
-'''
     def test_type(self):
         #Make sure type errors are raised when necessary
         self.assertRaises(TypeError, circle_area, "-2")
-'''
-
-
-
